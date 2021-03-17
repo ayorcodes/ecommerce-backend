0f87011734865e9f3c9cec192b43a1c1afdbb901
Ayomide <adeayo35@gmail.com>
2021-03-17 02:00:51 +0100

    reviews module

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,20 @@
-export class CreateReviewDto {}
+import { ApiHideProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateReviewDto {
+  // @IsNotEmpty()
+  @ApiHideProperty()
+  userId: string;
+
+  @IsNotEmpty()
+  productId: string;
+
+  @IsNotEmpty()
+  title: string;
+
+  @IsNotEmpty()
+  body: string;
+
+  @IsNotEmpty()
+  rating: number;
+}
--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,34 +1,34 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { AbstractController } from '../shared/controllers/abstract-controller.controller';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Reviews')
 @Controller('reviews')
-export class ReviewsController {
-  constructor(private readonly reviewsService: ReviewsService) {}
+export class ReviewsController extends AbstractController {
+  constructor(private readonly reviewsService: ReviewsService) {
+    super();
+    this.service = this.reviewsService;
+    this.name = 'Review';
+  }
 
   @Post()
   create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewsService.create(createReviewDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.reviewsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewsService.findOne(+id);
+    return super.create(createReviewDto);
   }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
-    return this.reviewsService.update(+id, updateReviewDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reviewsService.remove(+id);
+    return super.update(id, updateReviewDto);
   }
 }
