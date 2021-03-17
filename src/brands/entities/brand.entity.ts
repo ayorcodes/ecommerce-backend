7e4eac2293a1db87e84e20cd57c70476db6b612d
Ayomide <adeayo35@gmail.com>
2021-03-17 01:59:43 +0100

    brand entity

--- src/brands/entities/brand.entity.ts
+++ src/brands/entities/brand.entity.ts
@@ -1,6 +1,9 @@
-import { Entity } from "typeorm";
-import { BasicEntity } from "../../shared/entities/basic-entity";
-
+import { Entity, ManyToOne } from 'typeorm';
+import { Product } from '../../products/entities/product.entity';
+import { BasicEntity } from '../../shared/entities/basic-entity';
 
 @Entity('brands')
-export class Brand extends BasicEntity{}
+export class Brand extends BasicEntity {
+  // @ManyToOne(() => Product, (product) => product.brands)
+  // product: Product;
+}
