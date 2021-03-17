2903000257eb60203fce4dcf6e8f12551a5f39ba
Ayomide <adeayo35@gmail.com>
2021-03-17 02:01:58 +0100

    resolve dependents

--- src/shared/services/abstract-service.service.ts
+++ src/shared/services/abstract-service.service.ts
@@ -1,5 +1,6 @@
 import { NotFoundException } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { EntityTarget, getRepository, Repository } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
 import { AbstractPaginationDto } from '../dto/abstract-pagination.dto';
 import { PaginateItems } from '../response.transformer';
 
@@ -40,5 +41,24 @@ export class AbstractService {
     await this.findOne(id);
     await this.repository.delete(id);
     return null;
+  }
+
+  async resolveRelationships<T>(
+    payload: any[],
+    /* entity: EntityTarget<T> */ entity: any,
+  ) {
+    // let entity: T;
+    // console.log({
+    //   entity,
+    // });
+    const data = [];
+    for (const id of payload) {
+      const item = await getRepository(entity).findOne({ where: { id } });
+      if (item) {
+        data.push(item);
+      }
+    }
+    // console.log(data);
+    return data;
   }
 }
